1733ca57d1770a5105b1480553ddb5a56af99777
Phillip Burleigh <pburle2@illinois.edu>
2021-10-02 21:45:42 -0500

    Add HackDFW logo and gametwo screen button

---
+++ Assets/Scripts/UI Scripts/GameTwoInstructions.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameTwoInstructions : MonoBehaviour
+{
+    public void LoadGameTwoInstructions()
+    {
+        SceneManager.LoadScene("Type 2 Game Instructions", LoadSceneMode.Additive);
+    }
+}
