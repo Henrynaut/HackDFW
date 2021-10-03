8dd8d8f66cb1f5ae7c196bd1148875d9c018bb4a
Henrynaut <neilmchenry@gmail.com>
2021-10-03 01:06:36 -0500

    Created Balloon Manager

---
+++ Assets/Scripts/Balloon/BalloonManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonManager : MonoBehaviour
+{
+
+    public GameObject[] BalloonPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
