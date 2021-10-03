4bd927e34cfa23d9d83afb570d662c7ead793966
Phillip Burleigh <pburle2@illinois.edu>
2021-10-02 20:07:45 -0500

    Add Type1 instructions screen

---
+++ Assets/Scripts/UI Scripts/Type1Button.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Type1Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadType1Game()
+    {
+        SceneManager.LoadScene("Type 1 Game Instructions", LoadSceneMode.Additive);
+    }
+}
